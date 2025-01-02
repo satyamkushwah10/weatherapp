daec2d97e939ea2c3a2684ccb9fedeaaa1f6531a
Satyam <satyamkushwah016@gmail.com>
2025-01-02 14:52:10 +0530

    change title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import AppRoute from "./route/appRoutes";
-
+import logo from "../src/img/cloud_6628331.png"
 function App() {
     return (
         <>
+            <head>
+                <title>weather App</title>
+                <link rel="icon" type="image/jpg" href={logo}/>
+            </head>
             <AppRoute />
         </>
     );
